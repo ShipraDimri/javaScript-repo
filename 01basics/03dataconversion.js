9c20790adbb4330ba81b6387ea923c93c5e90772
ShipraDimri <shipradimri18@gmail.com>
2026-02-28 15:37:21 +0000

    knowing dataconversion

---
+++ 01basics/03dataconversion.js
@@ -0,0 +1,16 @@
+let d1=true
+console.log(typeof(d1))
+let cond1=Number(d1);
+console.log(typeof cond1);
+console.log(cond1);
+//"33"=>33
+//"33abc"=>NaN
+//true=>1;false=>0
+let islogged=0
+console.log(Boolean(islogged))
+let str="ship"
+console.log(Boolean(str))
+// in BOOLEAN empty string=>false;string=>true
+let someNumber=33
+console.log(String(someNumber))
+console.log(typeof(String(someNumber)))
