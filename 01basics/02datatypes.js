0c51c3e0bd80048b1bfd8e066fc627b74e2c95b9
ShipraDimri <shipradimri18@gmail.com>
2026-02-28 13:46:56 +0000

    knowing about datatypes

---
+++ 01basics/02datatypes.js
@@ -0,0 +1,9 @@
+console.log(3+3)
+console.log("addition is done")
+let name="shipra"
+let  age=18
+let isLogged=false
+let state;
+console.log(typeof state);
+console.log(typeof null)
+console.log(typeof undefined)
