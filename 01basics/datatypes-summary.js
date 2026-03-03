5a5bdd6137f1160b96e608712963f0fd7b4dc179
ShipraDimri <shipradimri18@gmail.com>
2026-03-03 20:13:56 +0530

    prmitive& non-pre-mitive

--- 01basics/datatypes-summary.js
+++ 01basics/datatypes-summary.js
@@ -1,6 +1,6 @@
 //premitive data types
 //7types:string,number,boolean,null,undefined,symbol,BigInt
-const score=10
+/*const score=10
 const scoreValue=10
 const isLogged=true
 const OutsideTemp=null
@@ -22,5 +22,20 @@ const MYfunction=function(){
 console.log(typeof(MYfunction));
 console.log(typeof(obj));
 console.log(typeof arr);
-console.log(typeof anotherId);
+console.log(typeof anotherId);*/
+
+//STACK(premitive) and HEAP(non-premitive) memory
+let myName="shipra"
+let anotherName=myName
+anotherName="dimri"
+console.log(myName);
+console.log(anotherName);
+let details1={
+    name:"shipra",
+    rollNo:49
+}
+let details2=details1;//same reference
+details2.name="dimri"
+console.log(details1.name);//dono me chnge ho jaega kyuki dono same reference point kr rhe h
+console.log(details2.name);
 
