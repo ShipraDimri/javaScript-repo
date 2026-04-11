ccf01b4a7cb9d2d70e7c6ba7432124660a5cf772
ShipraDimri <shipradimri18@gmail.com>
2026-04-11 23:09:53 +0530

    switch in js

---
+++ controlflow/switch.js
@@ -0,0 +1,27 @@
+/*
+switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}
+*/
+const month="may";
+switch(month){
+    case "january":
+        console.log("January");
+        break;
+    case "february":
+        console.log("February");
+        break;
+    case "march":
+        console.log("March");
+        break;
+    case "april":
+        console.log("April");
+        break;
+    default:
+        console.log("Invalid month");
+}
