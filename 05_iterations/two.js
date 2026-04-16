839e96c0a86f98b95e89ed7558df333d5477b74f
ShipraDimri <shipradimri18@gmail.com>
2026-04-16 23:05:17 +0530

    while and do while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,20 @@
+let i=0
+while(i<=5){
+//console.log(i);
+i+=2;
+
+}
+let myArr=['flash','superman','batman'];
+let j=0;
+while(j<myArr.length){
+    //console.log(myArr[j]);
+    j++;
+}
+console.log(`length of the arr is ${j}`);
+
+//do while loop
+let k=5;
+do{
+    console.log(`do while loop value: ${k}`);
+    k++;
+} while(k<=5);
