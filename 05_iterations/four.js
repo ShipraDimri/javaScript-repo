c77abba4dade7ed67e2a5ec02495475757177036
ShipraDimri <shipradimri18@gmail.com>
2026-04-17 14:27:43 +0530

    for of loop

---
+++ 05_iterations/four.js
@@ -0,0 +1,19 @@
+const myObject={
+    js:"JavaScript",
+    py:"Python",
+    java:"Java"
+}
+for (const key in myObject) {
+ //   console.log(myObject[key]);   
+}
+const programming=["js","py","java"];
+for (const key in programming) {
+    //console.log(programming[key]);
+}
+ const map=new Map();
+map.set("IN","India");
+map.set("US","United States");
+map.set("UK","United Kingdom");
+for (const key in map) {
+   // console.log(map[key]);
+}
